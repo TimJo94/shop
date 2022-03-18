05e1ec50ed86259c108a87d18f0b15c54a8356d3
TimJo94 <timurjorobekov@gmail.com>
2022-03-18 16:37:06 +0600

    Add product views

--- shop/urls.py
+++ shop/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import index_page, ProductsListView, ProductDetailsView
 
-from main.views import index_page
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page, name='index')
+    path('', index_page, name='index'),
+    path('products/', ProductsListView.as_view(), name='products-list'),
+    path('products/<int:pk>/', ProductDetailsView.as_view(), name='product-details'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
