276be8adb19d5ad79641bf92f03765d66fe1f408
TimJo94 <timurjorobekov@gmail.com>
2022-03-23 16:45:40 +0600

    CRUD

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from main.views import ProductDetailsView, ProductsListView, CreateProductView, UpdateProductView, DeleteProductView
+
+urlpatterns = [
+    path('create/', CreateProductView.as_view(), name='create-product'),
+    path('<slug:category_id>/', ProductsListView.as_view(), name='products-list'),
+    path('details/<int:pk>/', ProductDetailsView.as_view(), name='product-details'),
+    path('update/<int:pk>/', UpdateProductView.as_view(), name='update-product'),
+    path('delete/<int:pk>/', DeleteProductView.as_view(), name='delete-product'),
+
+
+
+]
